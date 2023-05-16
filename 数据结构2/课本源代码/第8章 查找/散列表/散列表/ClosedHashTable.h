52c159f3b7695724145c2834f89a31d81ba3ab69
MartinXuc <xuchicolson@163.com>
2023-05-16 11:50:19 +0800

    2023年5月16日11:50:17

--- 数据结构2/课本源代码/第8章 查找/散列表/散列表/ClosedHashTable.h
+++ 数据结构2/课本源代码/第8章 查找/散列表/散列表/ClosedHashTable.h
@@ -6,7 +6,10 @@
 #define EMPTY  0				// 空标记 
 #define USE    1				// 有元素标记 
 
+const float GR = (sqrt(5) - 1) / 2;//黄金分割比 Golden Ratio
+
 #include "Assistance.h"						// 辅助软件包
+#include "Triple.h"
 
 // 散列表类
 template <class ElemType, class KeyType>
@@ -17,18 +20,17 @@ protected:
 	ElemType *ht;								// 散列表
 	int *tag;								    // 标记数组 
 	int maxSize;								// 散列表容量
-	int p;										// 除留余数法的除数
 
 //	辅助函数:
-	int H(KeyType key) const;					// 散列函数
-	int Collision(KeyType key, int i) const;	// 处理冲突的函数
+	int H(KeyType row, KeyType col) const;					// 散列函数
+	int Collision(KeyType row, KeyType col, int i) const;	// 处理冲突的函数
 
 public:
 //  二叉树方法声明及重载编译系统默认方法声明:
-  	ClosedHashTable(int size, int divisor);			// 构造函数
+  	ClosedHashTable(int size);			// 构造函数
   	~ClosedHashTable();								// 析造函数
   	void Traverse(void (*Visit)(const ElemType &)) const;	// 遍历散列表
-	int Search(const KeyType &key) const ;	    // 查寻关键字为key的元素的下标 
+	int Search(const ElemType e) const ;	    // 查询e是否在表中
 	Status Insert(const ElemType &e);				// 插入元素e
 	bool Delete(const KeyType &key);			// 删除关键字为key的元素
   	ClosedHashTable(const ClosedHashTable<ElemType, KeyType> &t);	// 复制构造函数
@@ -38,28 +40,26 @@ public:
 
 // 散列表类的实现部分
 template <class ElemType, class KeyType>
-int ClosedHashTable<ElemType, KeyType>::H(KeyType key) const
+int ClosedHashTable<ElemType, KeyType>::H(KeyType row, KeyType col) const
 //操作结果: 返回散列地址
 {
-	return key % p;
+	return int(row * A + col * (1 - A)) % maxSize;
 }
 
 template <class ElemType, class KeyType>
-int ClosedHashTable<ElemType, KeyType>::Collision(KeyType key, int i) const
+int ClosedHashTable<ElemType, KeyType>::Collision(KeyType row, KeyType col, int i) const
 //操作结果: 返回第i次冲突的探查地址
 {
-	return (H(key) + i) % maxSize;
+	return (H(row, col) + i) % maxSize;
 }
 
 template <class ElemType, class KeyType>
-ClosedHashTable<ElemType, KeyType>::ClosedHashTable(int size, int divisor)
+ClosedHashTable<ElemType, KeyType>::ClosedHashTable(int size)
 // 操作结果: 以size为散列表容量, divisor为除留余数法的除数构造一个空的散表表
 {
 	maxSize = size;						// 赋值散列表容量
-	p = divisor;						// 赋值除数
 	ht = new ElemType[maxSize];			// 分配元素存储空间
 	tag = new int[maxSize];				// 分配标记数组存储空间
-
 	for (int i = 0; i < maxSize; i++)   // 将所有元素置空
 		tag[i] = 0;
 }
@@ -86,15 +86,15 @@ void ClosedHashTable<ElemType, KeyType>::Traverse(void (*Visit)(const ElemType &
 }
 
 template <class ElemType, class KeyType>
-int ClosedHashTable<ElemType, KeyType>::Search(const KeyType &key) const
+int ClosedHashTable<ElemType, KeyType>::Search(const ElemType e) const
 // 操作结果: 查寻关键字为key的元素的值,如果查找成功,返回元素在数组的下标,否则返回-1. 
 {
 	int c = 0;							// 冲突次数
-	int i = H(key);						// 元素的哈希函数值 
+	int i = H(e.row, e.col);						// 元素的哈希函数值 
 
-	while (c < maxSize &&	((tag[i] == USE && ht[i] != key) || tag[i] == DELETE))					
+	while (c < maxSize && ((tag[i] == USE && ht[i] != e) || tag[i] == DELETE))
 		// 冲突次数应小于maxSize且元素ht[pos]非空且关键字值不等
-		i = Collision(key, ++c);		// 求得下一个探查地址
+		i = Collision(e.row, e.col, ++c);		// 求得下一个探查地址
 
 	if (c >= maxSize || tag[i] == EMPTY)
 		return -1;						// 查找失败，返回-1 
@@ -107,14 +107,14 @@ Status ClosedHashTable<ElemType, KeyType>::Insert(const ElemType &e)
 // 操作结果: 在散列表中插入数据元素e,插入成功返回true,否则返回false
 {
    int c = 0;							// 冲突次数
-   int i = H(e);						// 元素的哈希函数值 
+   int i = H(e.row, e.col);						// 元素的哈希函数值 
    int k = -1;                     	// 插入位置 
 
    while (c < maxSize &&	((tag[i] == USE && ht[i] != e) || tag[i] == DELETE)) {					
       // 冲突次数应小于maxSize且元素ht[i]非空且关键字值不等
       if (k == -1 && tag[i] == DELETE)
          k = i;
-      i = Collision(e, ++c);		//求得下一个探查地址
+	  i = Collision(e.row, e.col, ++c);		//求得下一个探查地址
    }
 
 	if (c >= maxSize && k == -1)	   // 冲突次数应大于等于maxSize，说明哈希表已经放满 
@@ -148,7 +148,6 @@ ClosedHashTable<ElemType, KeyType>::ClosedHashTable(const ClosedHashTable<ElemTy
 // 操作结果：由散列表t构造新散列表--复制构造函数
 {
 	maxSize = t.maxSize;										// 散列表容量
-	p = t.p;										// 除留余数法的除数
 	ht = new ElemType[maxSize];							// 分配存储空间
 	tag = new int[maxSize];							// 分配存储空间
 
@@ -167,7 +166,6 @@ operator=(const ClosedHashTable<ElemType, KeyType> &t)
 		delete []ht;								// 释放当前散列表存储空间
 		delete []tag;
 		maxSize = t.maxSize;						// 散列表容量
-		p = t.p;									// 除留余数法的除数
 		ht = new ElemType[maxSize];					// 分配存储空间
 		tag = new int[maxSize];						// 分配存储空间
 
--- 数据结构2/课本源代码/第8章 查找/散列表/散列表/TestClosedHashTable.cpp
+++ 数据结构2/课本源代码/第8章 查找/散列表/散列表/TestClosedHashTable.cpp
@@ -2,61 +2,24 @@
 
 int main(void)
 {
-	ClosedHashTable<int, int> ht(20, 19);
-	int elem[] = {19, 14, 23, 1, 68, 20, 84, 27, 55, 11, 10, 79};
+	ClosedHashTable<Triple,int> ht(20000);
+	Triple t1(256, 344, 987);
+	Triple t2(137, 899, 654);
+	Triple t3(233, 455, 677);
+	Triple elem[] = { t1,t2,t3 };
 	int k, n = 12;
 	Status state;
 	
 	for (int i = 0; i < n; i++)
 		ht.Insert(elem[i]);				// 插入结点
 
-	cout << "初始Hash表(#位置表示空，*位置表示删除过元素):" << endl;
-	ht.Traverse(Write);
-	cout << endl;
-	
-    char c = 'x';
-    int x;
-
-    while (c != '0')	{
-        cout << endl << "1. 插入元素.";
-        cout << endl << "2. 查找元素.";
-        cout << endl << "3. 删除元素.";
-        cout << endl << "4. 显示Hash表.";
-		cout << endl << "0. 退出";
-		cout << endl << "选择功能(0~4):";
-		cin >> c;
-		switch (c) 	{
-		    case '1':
-            	cout << endl << "输入插入元素的值:";
-			    cin >> x;
-			    if ((state = ht.Insert(x)) == SUCCESS)
-                  	cout << endl << "插入成功.";
-              	else if (state == OVER_FLOW)
-                   	cout << endl << "表已满.";
-              	else 
-                   	cout << endl << "元素已存在.";
-			    break;
-           	case '2':
-              	cout << endl << "输入查找元素的值:";
-			    cin >> x;
-			    if ((k = ht.Search(x)) == -1)
-                   	cout << endl << "元素不存在.";
-              	else
-                   	cout << endl << "元素的位置是" << k;
-			    break;
-		    case '3':
-              	cout << endl << "输入删除元素的值:";
-			    cin >> x;
-			    if (ht.Delete(x))
-                   	cout << endl << "删除成功.";
-              	else
-                   	cout << endl << "元素不存在.";
-			    break;
-	         case '4':
-				cout << endl;
-			    ht.Traverse(Write<int>);
-				break;
-		}
+	for (int i = 1; i <= 3; i++) {
+		cout << "查看t" << i << "(" << elem[i].row << "," << elem[i].col << "," << elem[i].value << ")" << "在不在表中" << endl;
+		int pos = ht.Search(t1);
+		if (pos != -1)
+			cout << "找到了！，t1在表的位置为：" << endl;
+		else
+			cout << "没找到！" << endl;
 	}
 	
 	system("PAUSE");			
